8a81f2690b649a665ca183cf11587a4c0e862fdf
Sven Willenbuecher <sven.willenbuecher@gmx.de>
2025-05-11 13:36:59 +0200

    apply perltidy rules

--- lib/Data/Path.pm
+++ lib/Data/Path.pm
@@ -1,114 +1,120 @@
-package Data::Path;
-
 use 5.006001;
 use strict;
 use warnings;
-use Scalar::Util qw/reftype blessed/;
-use Carp;
+
+package Data::Path;
+
+use Scalar::Util qw( reftype blessed );
+use Carp         qw( croak );
 
 our $VERSION = '1.4.1';
 
 sub new {
-	my ($class,$data,$callback)=@_;
-	$callback||={};
-	my $self=
-		{ data     => $data
+  my ( $class, $data, $callback ) = @_;
+  $callback ||= {};
+  my $self = {
+    data => $data
 
-		# set call backs to default if not given
-		, callback =>
-			{ key_does_not_exist            => $callback->{key_does_not_exist} ||
-				sub {
-					my ($data, $key, $index, $value, $rest )=@_;
-					croak "key $key does not exists\n";
-				}
+      # set call backs to default if not given
+    ,
+    callback => {
+      key_does_not_exist => $callback->{ key_does_not_exist }
+        || sub {
+        my ( $data, $key, $index, $value, $rest ) = @_;
+        croak "key $key does not exists\n";
+        }
 
-			, index_does_not_exist         => $callback->{index_does_not_exist} ||
-				sub {
-					my ($data, $key, $index, $value, $rest )=@_;
-					croak "key $key\[$index\] does not exists\n";
-				}
+      ,
+      index_does_not_exist => $callback->{ index_does_not_exist }
+        || sub {
+        my ( $data, $key, $index, $value, $rest ) = @_;
+        croak "key $key\[$index\] does not exists\n";
+        }
 
-			, retrieve_index_from_non_array => $callback->{retrieve_index_from_non_array} ||
-				sub {
-					my ($data, $key, $index, $value, $rest )=@_;
-					croak "trie to retrieve an index $index from a no array value (in key $key)\n";
-				}
+      ,
+      retrieve_index_from_non_array => $callback->{ retrieve_index_from_non_array }
+        || sub {
+        my ( $data, $key, $index, $value, $rest ) = @_;
+        croak "trie to retrieve an index $index from a no array value (in key $key)\n";
+        }
 
-			, retrieve_key_from_non_hash    => $callback->{retrieve_key_from_non_hash} ||
-				sub {
-					my ($data, $key, $index, $value, $rest )=@_;
-					croak "trie to retrieve a key from a no hash value (in key $key)\n";
-				}
-			, not_a_coderef_or_method    => $callback->{not_a_coderef_or_method} ||
-				sub {
-					my ($data, $key, $index, $value, $rest )=@_;
-					croak "tried to retrieve from a non-existant coderef or method: $key in $data";
-				}
-			}
+      ,
+      retrieve_key_from_non_hash => $callback->{ retrieve_key_from_non_hash }
+        || sub {
+        my ( $data, $key, $index, $value, $rest ) = @_;
+        croak "trie to retrieve a key from a no hash value (in key $key)\n";
+        },
+      not_a_coderef_or_method => $callback->{ not_a_coderef_or_method }
+        || sub {
+        my ( $data, $key, $index, $value, $rest ) = @_;
+        croak "tried to retrieve from a non-existant coderef or method: $key in $data";
+        }
+    }
 
-		};
-	return bless $self,$class;
+  };
+  return bless $self, $class;
 }
 
 sub get {
-	my ($self,$rkey,$data)=@_;
+  my ( $self, $rkey, $data ) = @_;
 
-	# set data to
-	$data||=$self->{data};
+  # set data to
+  $data ||= $self->{ data };
 
-	# get key till / or [
-	my $key = $1 if ( $rkey =~ s/^\/([^\/|\[]+)//o );
-    croak 'malformed path expression'
-        unless $key;
+  # get key till / or [
+  my $key = $1 if ( $rkey =~ s/^\/([^\/|\[]+)//o );
+  croak 'malformed path expression'
+    unless $key;
 
-    croak 'malformed array index request'
-        if $rkey =~ /^\[([^\d]*)\]/;
-	# check index for index
-	my $index = $1 if ( $rkey =~ s/^\[(\d+)\]//o );
+  croak 'malformed array index request'
+    if $rkey =~ /^\[([^\d]*)\]/;
+  # check index for index
+  my $index = $1 if ( $rkey =~ s/^\[(\d+)\]//o );
 
-	# set rest
-	my $rest  = $rkey;
+  # set rest
+  my $rest = $rkey;
 
-	# get key from data
-	my $value;
-    if ($key =~ s/(\(\))$//) {
-        $self->{callback}->{not_a_coderef_or_method}->($data, $key, $index, $value, $rest)
-            unless exists $data->{$key} or (blessed $data && $data->can($key));
+  # get key from data
+  my $value;
+  if ( $key =~ s/(\(\))$// ) {
+    $self->{ callback }->{ not_a_coderef_or_method }->( $data, $key, $index, $value, $rest )
+      unless exists $data->{ $key }
+      or ( blessed $data && $data->can( $key ) );
 
-        $value = $data->{$key}->() if (exists $data->{$key});
-        $value = $data->$key() if blessed $data && $data->can($key);
+    $value = $data->{ $key }->() if ( exists $data->{ $key } );
+    $value = $data->$key()       if blessed $data && $data->can( $key );
+  } else {
+    $value = $data->{ $key };
+  }
+
+  # croak if key does not exists and something after that is requested
+  $self->{ callback }->{ key_does_not_exist }->( $data, $key, $index, $value, $rest )
+    if not exists $data->{ $key } and $rest;
+
+  # check index
+  if ( defined $index ) {
+
+    # croak if index does not exists and something after that is requested
+    $self->{ callback }->{ index_does_not_exist }->( $data, $key, $index, $value, $rest )
+      if not exists $value->[ $index ] and $rest;
+
+    if ( reftype $value eq 'ARRAY' ) {
+      $value = $value->[ $index ];
     } else {
-	   $value = $data->{$key};
+      $self->{ callback }->{ retrieve_index_from_non_array }->( $data, $key, $index, $value, $rest );
     }
+  }
 
-    # croak if key does not exists and something after that is requested
-	$self->{callback}->{key_does_not_exist}->($data, $key, $index, $value, $rest)
-		if not exists $data->{$key} and $rest;
+  # check if last element is reached
+  if ( $rest ) {
+    if ( reftype $value eq 'HASH' || blessed $value ) {
+      $value = $self->get( $rest, $value );
+    } else {
+      $self->{ callback }->{ retrieve_key_from_non_hash }->( $data, $key, $index, $value, $rest );
+    }
+  }
 
-	# check index
-	if (defined $index) {
-
-		# croak if index does not exists and something after that is requested
-		$self->{callback}->{index_does_not_exist}->($data, $key, $index, $value, $rest)
-			if not exists $value->[$index] and $rest;
-
-		if ( reftype $value eq 'ARRAY' ) {
-			$value=$value->[$index];
-		} else {
-			$self->{callback}->{retrieve_index_from_non_array}->($data, $key, $index, $value, $rest);
-		}
-	}
-
-	# check if last element is reached
-	if ($rest) {
-		if ( reftype $value eq 'HASH' || blessed $value ) {
-			$value=$self->get($rest,$value);
-		} else {
-			$self->{callback}->{retrieve_key_from_non_hash}->($data, $key, $index, $value, $rest);
-		}
-	}
-
-	return $value;
+  return $value;
 }
 
 1;
--- t/Data-Path.t
+++ t/Data-Path.t
@@ -1,108 +1,90 @@
+use strict;
+use warnings;
+
 use Test::More tests => 20;
 use Test::Exception;
-use Test::MockObject;
+use Test::MockObject ();
 
 BEGIN {
-    use lib qw(lib);
-    use_ok('Data::Path')
+  use_ok( 'Data::Path' )
+}
+
+my $hash = {
+  scalar => 'scalar_value',
+  array  => [ qw( array_value0 array_value1 array_value2 array_value3) ],
+  hash   => {
+    hash1 => 'hash_value1',
+    hash2 => 'hash_value2'
+  },
+  complex => { level2 => [ { level3_0 => [ 'level4_0', { level4_1 => { level5 => 'huhu' } }, 'level4_2' ] } ] },
+  method  => sub { return 'sub val'; }
+
 };
 
+my $a = Data::Path->new( $hash );
 
-my $hash=
-    { scalar => 'scalar_value'
-    , array  =>
-        [ qw( array_value0 array_value1 array_value2 array_value3)
-        ]
-    , hash   =>
-        {  hash1 => 'hash_value1'
-        ,  hash2 => 'hash_value2'
-        }
-    , complex =>
-        { level2 =>
-            [ { level3_0 =>
-                [ 'level4_0'
-                , { level4_1 => { level5 => 'huhu' }
-                  }
-				, 'level4_2'
-                ]
-              }
-            ]
-        }
-    , method => sub {return 'sub val';}
+ok( $a );
 
-    };
+my $v = '';
 
-my $a=Data::Path->new($hash);
+$v = $a->get( '/scalar' );
+ok( $v eq 'scalar_value', " value=$v" );
 
-ok ( $a );
+$v = $a->get( '/array[0]' );
+ok( $v eq 'array_value0', " value=$v" );
 
-my $v='';
+$v = $a->get( '/hash/hash1' );
+ok( $v eq 'hash_value1', " value=$v" );
 
-$v=$a->get('/scalar');
-ok ( $v eq 'scalar_value' , " value=$v");
+$v = $a->get( '/complex/level2[0]/level3_0[0]' );
+ok( $v eq 'level4_0', " value=$v" );
 
-$v=$a->get('/array[0]');
-ok ( $v eq 'array_value0' , " value=$v");
+$v = $a->get( '/complex/level2[0]/level3_0[2]' );
+ok( $v eq 'level4_2', " value=$v" );
 
-$v=$a->get('/hash/hash1');
-ok ( $v eq 'hash_value1'  , " value=$v");
+$v = $a->get( '/complex/level2[0]/level3_0[1]/level4_1/level5' );
+ok( $v eq 'huhu', " value=$v" );
 
-$v=$a->get('/complex/level2[0]/level3_0[0]');
-ok ( $v eq 'level4_0'     , " value=$v");
+eval { $a->get( '/complex/level2[99]/level3_0[1]/level4_1/level5' ); };
+ok( $@ =~ /does not exists/, " check error_msg = $@" );
 
-$v=$a->get('/complex/level2[0]/level3_0[2]');
-ok ( $v eq 'level4_2'     , " value=$v");
+eval { $a->get( '/complex/level2[0]/level3_1[1]/level4_1/level5' ); };
+ok( $@ =~ /does not exists/, " check error_msg = $@" );
 
-$v=$a->get('/complex/level2[0]/level3_0[1]/level4_1/level5');
-ok ( $v eq 'huhu' ," value=$v");
+$v = $a->get( '/complex/level2[0]/level3_0[1]/level4_1/level5_not_exists' ) || 'UNDEF';
+ok( $v eq 'UNDEF', " value=$v" );
 
-eval {
-	$a->get('/complex/level2[99]/level3_0[1]/level4_1/level5');
-};
-ok ( $@ =~/does not exists/  ," check error_msg = $@");
+$v = $a->get( '/complex/level2[0]/level3_0[99]' ) || 'UNDEF';
+ok( $v eq 'UNDEF', " value=$v" );
 
-eval {
-	$a->get('/complex/level2[0]/level3_1[1]/level4_1/level5');
-};
-ok ( $@ =~/does not exists/  ," check error_msg = $@");
+$v = $a->get( '/complex/level2[0]/level3_0[2]' ) || 'UNDEF';
+ok( $v eq 'level4_2', " value=$v" );
 
-$v=$a->get('/complex/level2[0]/level3_0[1]/level4_1/level5_not_exists') || 'UNDEF';
-ok ( $v eq 'UNDEF' ," value=$v");
+my $b = Data::Path->new(
+  $hash,
+  {
+    'key_does_not_exist'   => sub { die 'callback_error_key' },
+    'index_does_not_exist' => sub { die 'callback_error_index' }
+  }
+);
+eval { $b->get( '/complex/home/' ); };
+ok( $@ =~ /callback_error_key/, " check error_msg = $@" );
 
-$v=$a->get('/complex/level2[0]/level3_0[99]') || 'UNDEF';
-ok ( $v eq 'UNDEF' ," value=$v");
+eval { $b->get( '/complex/level2[99]/level3_0' ); };
+ok( $@ =~ /callback_error_index/, " check error_msg = $@" );
+my $obj = Test::MockObject->new( {} );
 
-$v=$a->get('/complex/level2[0]/level3_0[2]') || 'UNDEF';
-ok ( $v eq 'level4_2' ," value=$v");
+$obj->mock( 'method2' => sub { 'method2 val' } );
+my $b2 = Data::Path->new( $obj );
+is( $b->get( '/method()' ),         $hash->{ method }->(), "subroutine returned" );
+is( $b2->get( '/method2()', $obj ), $obj->method2(),       "method returned" );
 
+my $deep_method = { foo => $obj };
 
-my $b=Data::Path->new($hash,
-	{ 'key_does_not_exist'=>sub{ die 'callback_error_key' } 
-	, 'index_does_not_exist'=>sub{ die 'callback_error_index' } 
-	} );
-eval {
-	$b->get('/complex/home/');
-};
-ok ( $@ =~/callback_error_key/  ," check error_msg = $@");
+$b = Data::Path->new( $deep_method );
+is( $b->get( '/foo/method2()' ), $obj->method2(), "deep method returned" );
 
-eval {
-	$b->get('/complex/level2[99]/level3_0');
-};
-ok ( $@ =~/callback_error_index/  ," check error_msg = $@");
-my $obj = Test::MockObject->new({});
-
-$obj->mock('method2' => sub {'method2 val'});
-my $b2 = Data::Path->new($obj);
-is($b->get('/method()'), $hash->{method}->(), "subroutine returned"); 
-is($b2->get('/method2()', $obj), $obj->method2(), "method returned"); 
-
-my $deep_method = { foo => $obj};
-
-$b = Data::Path->new($deep_method);
-is($b->get('/foo/method2()'), $obj->method2(), "deep method returned");
-
-throws_ok { Data::Path->new( { foo => 1 } )->get('goo') }
-    qr/malformed path expression/, 'malformed path expression throws an error'; 
-throws_ok { Data::Path->new( { foo => [1,2] } )->get('/foo[]') }
-    qr/malformed array index request/, 'malformed array path expression throws an error'; 
-
+throws_ok { Data::Path->new( { foo => 1 } )->get( 'goo' ) }
+qr/malformed path expression/, 'malformed path expression throws an error';
+throws_ok { Data::Path->new( { foo => [ 1, 2 ] } )->get( '/foo[]' ) }
+qr/malformed array index request/, 'malformed array path expression throws an error';
